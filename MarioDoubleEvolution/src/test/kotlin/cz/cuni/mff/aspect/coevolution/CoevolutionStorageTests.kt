fda2f80ecf7b79da041e5f373d943aeafa69633e
Julius Flimmel <flimmelj@gmail.com>
2020-06-03 15:52:57 +0200

    Fix in unit tests

--- MarioDoubleEvolution/src/test/kotlin/cz/cuni/mff/aspect/coevolution/CoevolutionStorageTests.kt
+++ MarioDoubleEvolution/src/test/kotlin/cz/cuni/mff/aspect/coevolution/CoevolutionStorageTests.kt
@@ -8,6 +8,8 @@ import cz.cuni.mff.aspect.mario.controllers.MarioController
 import cz.cuni.mff.aspect.visualisation.charts.evolution.EvolutionLineChart
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.mockkStatic
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
@@ -21,6 +23,7 @@ class CoevolutionStorageTests {
     private val mockGenerator = mockk<LevelGenerator>()
     private val mockLevelGeneratorEvolution = mockk<LevelGeneratorEvolution<LevelGenerator>>()
     private val mockControllerEvolution = mockk<ControllerEvolution>()
+    private val mockCoevolutionTimer = mockk<CoevolutionTimer>()
     private val mockLastGeneratorsPopulation = List(10) {
         PMPLevelGenerator(DoubleArray(PMPLevelGenerator.PROBABILITIES_COUNT) { 0.1 }, 50)
     }
@@ -28,8 +31,12 @@ class CoevolutionStorageTests {
 
     @BeforeEach
     fun beforeEach() {
+        mockkObject(CoevolutionTimer)
+
         every { mockControllerEvolution.chart } returns mockEvolutionChart
         every { mockLevelGeneratorEvolution.chart } returns mockEvolutionChart
+        every { mockCoevolutionTimer.store(any()) } returns Unit
+        every { CoevolutionTimer.loadFromFile(any()) } returns mockCoevolutionTimer
     }
 
     private val mockSettings = CoevolutionSettings(
@@ -54,15 +61,15 @@ class CoevolutionStorageTests {
     fun `test store and load doesn't crash`() {
         val generation = 5
 
-        (1..generation).forEach { CoevolutionStorage.storeState(mockSettings, it, mockController, mockGenerator, mockLastGeneratorsPopulation) }
+        (1..generation).forEach { CoevolutionStorage.storeState(mockSettings, it, mockController, mockGenerator, mockLastGeneratorsPopulation, mockCoevolutionTimer) }
         CoevolutionStorage.loadState(mockSettings)
     }
 
     @Test
     fun `test correct last stored generation number is computed`() {
-        CoevolutionStorage.storeState(mockSettings, 1, mockController, mockGenerator, mockLastGeneratorsPopulation)
-        CoevolutionStorage.storeState(mockSettings, 2, mockController, mockGenerator, mockLastGeneratorsPopulation)
-        CoevolutionStorage.storeState(mockSettings, 3, mockController, mockGenerator, mockLastGeneratorsPopulation)
+        CoevolutionStorage.storeState(mockSettings, 1, mockController, mockGenerator, mockLastGeneratorsPopulation, mockCoevolutionTimer)
+        CoevolutionStorage.storeState(mockSettings, 2, mockController, mockGenerator, mockLastGeneratorsPopulation, mockCoevolutionTimer)
+        CoevolutionStorage.storeState(mockSettings, 3, mockController, mockGenerator, mockLastGeneratorsPopulation, mockCoevolutionTimer)
 
         val loadedState = CoevolutionStorage.loadState(mockSettings)
 
@@ -77,7 +84,7 @@ class CoevolutionStorageTests {
         val generatorsProbabilities = List(populationSize) { DoubleArray(PMPLevelGenerator.PROBABILITIES_COUNT) { random.nextDouble() } }
         val actualGeneratorsPopulation = List(10) { PMPLevelGenerator(generatorsProbabilities[it], 50) }
 
-        CoevolutionStorage.storeState(mockSettings, 3, mockController, mockGenerator, actualGeneratorsPopulation)
+        CoevolutionStorage.storeState(mockSettings, 3, mockController, mockGenerator, actualGeneratorsPopulation, mockCoevolutionTimer)
         val loadedState = CoevolutionStorage.loadState(mockSettings)
 
         val loadedGeneratorsPopulation = loadedState.latestGeneratorsPopulation
